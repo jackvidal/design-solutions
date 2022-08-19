bed4099c1483e716e33974ce3f33fa1a281e7c3f
jack <jack@borntoconvert.com>
2022-08-19 19:51:46 +0300

    Added project files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <header className='main-header'>
+      <div className="container">
+        <a href="#" className="main-logo"><img src="./src/assets/logo.svg" alt="logo" /></a>
+        <button className='button'>Free Consultaion</button>
+      </div>
+     </header>
+     <main>
+      <section className='hero-grid'>
+        
+      </section>
+
+
+
+
+     </main>
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
